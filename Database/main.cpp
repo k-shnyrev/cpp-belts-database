a93a8a4d8002e8da2847cb1bd6a3677da8303fcf
Kirill Shnyrev <k.shnyrev@mail.ru>
2021-02-07 20:46:56 +0300

    Initial Commit

---
+++ Database/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Database
+//
+//  Created by Kirill Shnyrev on 07.02.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
